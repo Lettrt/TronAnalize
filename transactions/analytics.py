f6a617ba45cec441d0bf504f6e54ac0c4a883ee8
Alex <ak555110689@gmail.com>
2023-11-14 21:39:43 +0600

    avg & std numpy

--- transactions/analytics.py
+++ transactions/analytics.py
@@ -1,17 +1,26 @@
-from django.db.models import Avg, StdDev
-from .models import Transaction
+import os
+import sys
+from pathlib import Path
+
+project_path = Path('/home/letterbe/develops/blockchain/tronAnaliz')
+sys.path.append(str(project_path))
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+import django
+django.setup()
+
+import numpy as np
+from transactions.models import Transaction
 
 def calculate_transaction_statistics():
-    transactions = Transaction.objects.filter(amount__gte=10)
-    
-    average = transactions.aggregate(Avg('amount'))['amount__avg']
-    stddev = transactions.aggregate(StdDev('amount'))['amount__stddev']
+    amounts = list(Transaction.objects.filter(amount__gte=10).values_list('amount', flat=True)) # gte - greater than or equal to
+    average = np.mean(amounts)
+    stddev = np.std(amounts)
     return average, stddev
 
 def find_anomalies(average, stddev, multiplier=3):
     lower_threshold = average - (multiplier * stddev)
     upper_threshold = average + (multiplier * stddev)
-    
     anomalies = Transaction.objects.filter(
         amount__lt=lower_threshold
     ) | Transaction.objects.filter(
